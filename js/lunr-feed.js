017e123f0c0ee527842003daabcca5faa340b3a5
Rickie Sherman <rickiesherman@me.com>
2017-05-22 11:37:18 -0700

    fixing height of sidebar

--- js/lunr-feed.js
+++ js/lunr-feed.js
@@ -22,7 +22,7 @@ var store = [{% for post in site.posts %}{
   "title": {{post.title | jsonify | }},
   "link": {{ site.baseurl | append: post.url | jsonify }},
   "category": {{ post.categories.[0] | jsonify }},
-  "excerpt": {{ post.content | strip_html | truncatewords: 10 | jsonify }}
+  "excerpt": {{ post.content | strip_html | truncatewords: 9 | jsonify }}
 }{% unless forloop.last %},{% endunless %}{% endfor %}]
 
 $(document).ready(function() {
