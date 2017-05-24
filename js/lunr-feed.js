a931551a6fffa46d9ced26cc5d93dc9d0fa7edf3
Rickie Sherman <rickiesherman@me.com>
2017-05-24 12:30:15 -0700

    fixing height issues and other errors

--- js/lunr-feed.js
+++ js/lunr-feed.js
@@ -19,10 +19,10 @@ index.add({
 });{% assign count = count | plus: 1 %}{% endfor %}
 
 var store = [{% for post in site.posts %}{
-  "title": {{post.title | jsonify | }},
-  "link": {{ site.baseurl | append: post.url | jsonify }},
+  "title": {{post.title | jsonify}},
+  "link": {{ site.baseurl | append: post.url | jsonify}},
   "category": {{ post.categories.[0] | jsonify }},
-  "excerpt": {{ post.content | strip_html | truncatewords: 9 | jsonify }}
+  "excerpt": {{ post.content | strip_html | truncatewords: 9 | jsonify}}
 }{% unless forloop.last %},{% endunless %}{% endfor %}]
 
 $(document).ready(function() {
