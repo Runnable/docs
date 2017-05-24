d28a692797d794834d310dd3f714bfd824dd5d20
Rickie Sherman <rickiesherman@me.com>
2017-05-24 15:15:08 -0700

    cleaning up lunr-feed

--- js/lunr-feed.js
+++ js/lunr-feed.js
@@ -11,18 +11,18 @@ var index = lunr(function () {
 
 {% assign count = 0 %}{% for post in site.posts %}
 index.add({
-  title: {{post.title | jsonify}},
-  category: {{post.categories.[0] | jsonify}},
-  content: {{post.content | strip_html | jsonify}},
-  tags: {{post.tags | jsonify}},
-  id: {{count}}
+  title: {{ post.title | jsonify }},
+  category: {{ post.categories.[0] | jsonify }},
+  content: {{ post.content | strip_html | jsonify }},
+  tags: {{ post.tags | jsonify }},
+  id: {{ count }}
 });{% assign count = count | plus: 1 %}{% endfor %}
 
 var store = [{% for post in site.posts %}{
   "title": {{post.title | jsonify}},
-  "link": {{ site.baseurl | append: post.url | jsonify}},
+  "link": {{ site.baseurl | append: post.url | jsonify }},
   "category": {{ post.categories.[0] | jsonify }},
-  "excerpt": {{ post.content | strip_html | truncatewords: 9 | jsonify}}
+  "excerpt": {{ post.content | strip_html | truncatewords: 9 | jsonify }}
 }{% unless forloop.last %},{% endunless %}{% endfor %}]
 
 $(document).ready(function() {
